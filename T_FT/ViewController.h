5c0d58ce318bff62d241bb6fcc64b20dd77271cc
James <james@jamesdeimac.local>
2018-06-03 14:37:48 +0800

    Initial Commit

---
+++ T_FT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  T_FT
+//
+//  Created by James on 2018/6/3.
+//  Copyright © 2018年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ T_FT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  T_FT
+//
+//  Created by James on 2018/6/3.
+//  Copyright © 2018年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
