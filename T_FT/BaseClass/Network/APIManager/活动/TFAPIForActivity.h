6de0d8ea8cef0e97fa5e0f1105f4a5ef302e5cfe
james <james.ltesim@gmail.com>
2018-07-24 16:00:16 +0800

    a

---
+++ T_FT/BaseClass/Network/APIManager/活动/TFAPIForActivity.h
@@ -0,0 +1,13 @@
+//
+//  TFAPIForActivity.h
+//  T_FT
+//
+//  Created by James on 2018/7/24.
+//  Copyright © 2018年 James. All rights reserved.
+//
+
+#import "TFAPIBaseManager.h"
+
+@interface TFAPIForActivity : TFAPIBaseManager
+
+@end
---
+++ T_FT/BaseClass/Network/APIManager/活动/TFAPIForActivity.m
@@ -0,0 +1,13 @@
+//
+//  TFAPIForActivity.m
+//  T_FT
+//
+//  Created by James on 2018/7/24.
+//  Copyright © 2018年 James. All rights reserved.
+//
+
+#import "TFAPIForActivity.h"
+
+@implementation TFAPIForActivity
+
+@end
